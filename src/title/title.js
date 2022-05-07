75c560f9b3c1fb2241003f5808a8287acc444091
Vladimir Kozinsky <kozinskytxc@gmail.com>
2022-05-07 21:54:21 +0400

    fix: del dev files

--- src/title/title.js
+++
@@ -1,8 +0,0 @@
-function Title() {
-    let title = document.createElement("h1");
-    title.className = "title";
-    title.textContent = "Виртуальная клавиатура";
-    return title;
-}
-
-export default Title;
